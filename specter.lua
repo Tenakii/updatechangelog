9fe3799ca66f7c798d87b8b0c63ff2baca8a8881
tenaaki <87340697+Tenakii@users.noreply.github.com>
2022-04-16 23:05:20 +0100

    Create specter.lua

---
+++ specter.lua
@@ -0,0 +1,33 @@
+[ version 0.0.3 ] 
+
+[ + ] 
+
+[ new ui library ]
+
+[ rage bot ]
+
+[ revamped esp ] 
+
+[ improved target strafe ] 
+
+[ improved bullet tracers ] ,
+
+[ aimbot consistancy ]
+
+[ - fixes - ] ,
+
+[ silent aim - use fov ] 
+
+[ target strafe - keybinded ]
+
+[ - jump cooldown - ] 
+
+[ target strafe flinger ] 
+
+
+[ - ] 
+
+[ fake Lag ] * experimental , ( not working in 0.0.3 )
+
+
+
